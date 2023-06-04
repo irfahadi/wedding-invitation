88c0e51bc9933ad88d2d27f4e1625a0bdb5f14c4
Christianto Chen <christiantochen@users.noreply.github.com>
2023-06-04 22:15:41 +0700

    chore: update

--- components/home/weddingWishes.tsx
+++ components/home/weddingWishes.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Title from "../title";
 
 export default function WeddingWishes({
@@ -10,6 +11,18 @@ export default function WeddingWishes({
   return (
     <article className={className}>
       {children || <Title>WEDDING WISHES</Title>}
+
+      <div className="text-center text-sm font-normal">
+        Your wishes are our blessing to start a<br />
+        new awesome journey ahead!
+      </div>
+
+      <button
+        className="flex justify-center text-sm text-white mt-12 px-6 py-2 rounded-full cursor-pointer"
+        style={{ background: "#50657F" }}
+      >
+        Send
+      </button>
     </article>
   );
 }
