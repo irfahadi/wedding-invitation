653b0a88e7f6e29b8b98828d2faf289bd5d1697d
Christianto Chen <christiantochen@users.noreply.github.com>
2023-06-14 21:43:31 +0700

    chore: update

--- components/home/weddingWishes.tsx
+++ components/home/weddingWishes.tsx
@@ -1,10 +1,16 @@
 import Title from "../title";
-import { FaPencilAlt, FaUser } from "react-icons/fa";
+import {
+  FaPencilAlt,
+  FaRegCalendarCheck,
+  FaRegCalendarTimes,
+  FaUser,
+  FaUserFriends,
+} from "react-icons/fa";
 import { FormEvent, useEffect, useRef, useState } from "react";
-import { toast } from "react-hot-toast";
 import Papa from "papaparse";
 import { useCookieState } from "use-cookie-state";
 import TransitionWrapper from "../transitionWrapper";
+import { toast } from "react-hot-toast";
 
 export default function WeddingWishes({
   className,
@@ -18,6 +24,8 @@ export default function WeddingWishes({
   const [loading, setLoading] = useState(false);
   const formRef = useRef<HTMLFormElement>(null);
   const [data, setData] = useState<any[]>();
+  const [attend, setAttend] = useState<string>("");
+  const [total, setTotal] = useState<string>("");
 
   useEffect(() => {
     Papa.parse(
@@ -62,6 +70,9 @@ export default function WeddingWishes({
         } else {
           setData([result]);
         }
+
+        setTotal("");
+        setAttend("");
       })
       .catch((e) => toast.error(e.message));
   };
@@ -111,6 +122,68 @@ export default function WeddingWishes({
               style={{ resize: "none" }}
             />
           </div>
+          <div
+            className="flex justify-center px-2 space-x-2 border-b relative"
+            style={{ borderColor: "#E9D3AE" }}
+          >
+            {attend === "1" ? (
+              <FaRegCalendarCheck
+                className="mt-3"
+                style={{ color: "#E9D3AE" }}
+              />
+            ) : (
+              <FaRegCalendarTimes
+                className="mt-3"
+                style={{ color: "#E9D3AE" }}
+              />
+            )}
+            <select
+              name="attending"
+              className="p-2 w-full bg-transparent outline-none"
+              style={{
+                color: attend === "" ? "#a9a9a9" : "rgb(38, 34, 98)",
+              }}
+              required
+              onChange={(e) => {
+                setAttend(e.target.value);
+
+                if (e.target.value === "0") {
+                  setTotal("");
+                }
+              }}
+              value={attend}
+            >
+              <option disabled value="">
+                Will you attend the wedding ?
+              </option>
+              <option value="1">Yes, I will gladly attend</option>
+              <option value="0">No, I can`t attend the wedding</option>
+            </select>
+          </div>
+          {attend === "1" && (
+            <div
+              className="flex justify-center px-2 space-x-2 border-b relative"
+              style={{ borderColor: "#E9D3AE" }}
+            >
+              <FaUserFriends className="mt-3" style={{ color: "#E9D3AE" }} />
+              <select
+                name="total"
+                className="p-2 w-full bg-transparent outline-none"
+                style={{
+                  color: total === "" ? "#a9a9a9" : "rgb(38, 34, 98)",
+                }}
+                onChange={(e) => setTotal(e.target.value)}
+                required={attend === "1"}
+                value={total}
+              >
+                <option disabled value="">
+                  How many guests ?
+                </option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+              </select>
+            </div>
+          )}
           <button
             className="flex justify-center text-sm text-white mx-auto mt-6 mb-6 px-6 py-2 rounded-full cursor-pointer relative"
             style={{
